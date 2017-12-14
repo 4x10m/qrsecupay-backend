e1e492fb5f005a41d8c1a3aa06f57bde301eba1d
Evan <evan.garotadrian@gmail.com>
2017-12-14 23:06:27 +0100

    removed useless stuff

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var lessMiddleware = require('less-middleware');
 var cors = require('cors');
 
 var index = require('./routes/index');
@@ -15,17 +13,10 @@ app.use(cors());
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
